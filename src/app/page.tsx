ea8254957819e9ee898331e9503e9432a878d41d
Anton <124072404+WiZy991@users.noreply.github.com>
2025-10-04 15:00:00 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,29 @@ import Link from 'next/link'
 export default function HomePage() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
-      {/* SVG как фон */}
+      {/* Фон */}
       <img
         src="/nessi.svg"
         alt="Nessi Background"
         className="absolute inset-0 w-full h-full object-cover"
       />
 
-      {/* Вход */}
+      {/* --- Вход --- */}
       <Link href="/login">
-        <div className="absolute top-[50px] right-[180px] w-[100px] h-[40px] cursor-pointer" />
+        <div className="absolute top-[50px] right-[140px] w-[90px] h-[40px] cursor-pointer" />
       </Link>
 
-      {/* Регистрация */}
+      {/* --- Регистрация --- */}
       <Link href="/register">
-        <div className="absolute top-[50px] right-[40px] w-[150px] h-[40px] cursor-pointer" />
+        <div className="absolute top-[50px] right-[40px] w-[130px] h-[40px] cursor-pointer" />
       </Link>
 
-      {/* Бизнес */}
+      {/* --- Бизнес --- */}
       <Link href="/business">
         <div className="absolute top-[330px] left-[720px] w-[180px] h-[70px] cursor-pointer" />
       </Link>
 
-      {/* Таланты */}
+      {/* --- Таланты --- */}
       <Link href="/talents">
         <div className="absolute top-[330px] left-[930px] w-[180px] h-[70px] cursor-pointer" />
       </Link>
