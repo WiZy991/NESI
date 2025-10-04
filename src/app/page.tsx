1e50d42d3b86d4e2be2288b228c71a7f553101a7
Anton <124072404+WiZy991@users.noreply.github.com>
2025-10-04 17:37:10 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,62 +11,62 @@ export default function Home() {
             src="/nessi.svg"
             alt="Nessi Background"
             className="absolute inset-0 w-full h-full object-cover"
-            style={{ pointerEvents: "none" }}
+            style={{ pointerEvents: 'none' }}
           />
 
-          {/* Вход */}
+          {/* ВХОД */}
           <a aria-label="Вход" href="/login">
             <div
               className="absolute z-10 cursor-pointer"
               style={{
-                top: "2.7777777777777777%",
-                left: "84.47916666666667%",
-                width: "4.375%",
-                height: "3.148148148148148%",
+                top: '4.1%',
+                left: '80.5%',
+                width: '6%',
+                height: '3.2%',
               }}
             />
           </a>
 
-          {/* Регистрация */}
+          {/* РЕГИСТРАЦИЯ */}
           <a aria-label="Регистрация" href="/register">
             <div
               className="absolute z-10 cursor-pointer"
               style={{
-                top: "2.7777777777777777%",
-                left: "89.375%",
-                width: "8.333333333333332%",
-                height: "3.148148148148148%",
+                top: '4.1%',
+                left: '87.3%',
+                width: '9%',
+                height: '3.2%',
               }}
             />
           </a>
 
-          {/* Бизнес */}
+          {/* БИЗНЕС */}
           <a aria-label="Бизнес" href="/business">
             <div
               className="absolute z-10 cursor-pointer"
               style={{
-                top: "35%",
-                left: "31.25%",
-                width: "18.229166666666664%",
-                height: "5.555555555555555%",
+                top: '29.5%',
+                left: '31.5%',
+                width: '18.5%',
+                height: '7.2%',
               }}
             />
           </a>
 
-          {/* Таланты */}
+          {/* ТАЛАНТЫ */}
           <a aria-label="Таланты" href="/talents">
             <div
               className="absolute z-10 cursor-pointer"
               style={{
-                top: "35%",
-                left: "53.385416666666664%",
-                width: "23.4375%",
-                height: "5.555555555555555%",
+                top: '29.5%',
+                left: '53.7%',
+                width: '23.5%',
+                height: '7.2%',
               }}
             />
           </a>
         </div>
       </div>
     </main>
-  );
+  )
 }
