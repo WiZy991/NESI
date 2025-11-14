fc40b0d9dace7720e0e2d202306ce8fe29be44ca
Heyaski <rudenko.sa@dvfu.ru>
2025-11-14 15:16:48 +1000

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
 })
 const poppins = Poppins({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-	subsets: ['latin', 'cyrillic'],
+	subsets: ['latin'],
 	variable: '--font-poppins',
 	display: 'swap',
 })
